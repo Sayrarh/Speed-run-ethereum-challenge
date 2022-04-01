046fc90914ba9e99094e1e81dd0c977b568288e5
Oluwatosin Ajao <ajaotosinserah@gmail.com>
2022-04-01 22:28:16 +0000

    challenge 1

--- packages/hardhat/contracts/Staker.sol
+++ packages/hardhat/contracts/Staker.sol
@@ -22,7 +22,7 @@ contract Staker {
   uint256 public constant threshold = 1 ether;
 
   // Staking deadline
-  uint256 public deadline = block.timestamp + 30 seconds;
+  uint256 public deadline = block.timestamp + 72 hours;
   bool openForWithdraw = false;
   // Contract's Events
   event Stake(address indexed sender, uint256 amount);
